f74ad77c9bbcb381a91632f1e52ce829a3136338
shararthkp47 <sharathkp18@gmail.com>
2020-11-21 11:15:01 +0530

    connect FireBaseTest

--- app/src/main/java/com/example/freephoneapp/Login.java
+++ app/src/main/java/com/example/freephoneapp/Login.java
@@ -8,18 +8,23 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 public class Login extends AppCompatActivity {
 Button loginBtn;
 TextView signUpTv;
-
+    FireBaseTest fb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
           loginBtn=findViewById(R.id.loginbtn);
+        fb=new FireBaseTest();
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                fb.addToFirebase("message");
                 startActivity(new Intent(Login.this,FreeList.class));
              }
         });
@@ -27,6 +32,8 @@ TextView signUpTv;
         signUpTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Toast.makeText(Login.this,  fb.retrieveFromFirebase("message"), Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(Login.this,SignUp.class));
             }
         });
