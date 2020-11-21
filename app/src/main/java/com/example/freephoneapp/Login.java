f2ce809b970bfc0f39a34d208bbf290da1348c51
shararthkp47 <sharathkp18@gmail.com>
2020-11-21 15:30:56 +0530

    signin Login

--- app/src/main/java/com/example/freephoneapp/Login.java
+++ app/src/main/java/com/example/freephoneapp/Login.java
@@ -1,5 +1,7 @@
 package com.example.freephoneapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,35 +9,83 @@ import android.content.Intent;
  import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
 public class Login extends AppCompatActivity {
 Button loginBtn;
+EditText user,pass;
 TextView signUpTv;
-    FireBaseTest fb;
-    @Override
+boolean isUser;
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
           loginBtn=findViewById(R.id.loginbtn);
-        fb=new FireBaseTest();
+        user=findViewById(R.id.usernameet);
+        pass=findViewById(R.id.passwordet);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                fb.addToFirebase("message");
-                startActivity(new Intent(Login.this,FreeList.class));
-             }
+                validate();
+              }
         });
         signUpTv=findViewById(R.id.signuptv);
         signUpTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(Login.this,  fb.retrieveFromFirebase("message"), Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(Login.this,SignUp.class));
+                 startActivity(new Intent(Login.this,SignUp.class));
             }
         });
+    }
+    public  boolean validate()    {
+        try {
+            if (user.getText().toString().isEmpty() || pass.getText().toString().isEmpty()) {
+                Toast.makeText(Login.this, "please enter username/password", Toast.LENGTH_SHORT).show();
+            }else {
+                final FirebaseFirestore database = FirebaseFirestore.getInstance();
+                DocumentReference docRef = database.collection("userlist").document(user.getText().toString());
+                docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (value.exists()) {
+                            database.collection("userlist").document(user.getText().toString())
+                                    .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        DocumentSnapshot document = task.getResult();
+                                        if (task.isComplete()) {
+                                            String str_pass = document.get("PASSWORD").toString();
+                                            String str_name = document.get("NAME").toString();
+                                            if (user.getText().toString().equals(str_name) && pass.getText().toString().equals(str_pass)) {
+                                                startActivity(new Intent(Login.this, FreeList.class));
+                                            } else {
+                                                Toast.makeText(Login.this, "invalid login credentials", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(Login.this, "user does not exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return isUser;
     }
 }
--- app/src/main/java/com/example/freephoneapp/SignUp.java
+++ app/src/main/java/com/example/freephoneapp/SignUp.java
@@ -1,26 +1,97 @@
 package com.example.freephoneapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
 Button signUpBtn;
 TextView loginTv;
+EditText userName,setPass,email,phone,conPass;
+    boolean isUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        loginTv=findViewById(R.id.logintv);
-        loginTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SignUp.this,Login.class));
-            }
-        });
+        try {
+            signUpBtn = findViewById(R.id.signupbtn);
+
+            loginTv = findViewById(R.id.logintv);
+            userName = findViewById(R.id.usernameet);
+            setPass = findViewById(R.id.passwordet);
+            email = findViewById(R.id.emailet);
+            phone = findViewById(R.id.phoneet);
+            conPass = findViewById(R.id.confpasset);
+
+            loginTv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(SignUp.this, Login.class));
+                }
+            });
+
+            signUpBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (userName.getText().toString().isEmpty() || email.getText().toString().isEmpty()
+                            || phone.getText().toString().isEmpty() || setPass.getText().toString().isEmpty()
+                            || conPass.getText().toString().isEmpty()) {
+                        Toast.makeText(SignUp.this, "please enter all fields", Toast.LENGTH_SHORT).show();
+                    } else if (!setPass.getText().toString().equals(conPass.getText().toString())) {
+                        Toast.makeText(SignUp.this, "password not matching", Toast.LENGTH_SHORT).show();
+                    } else if (validation()) {
+                        Toast.makeText(SignUp.this, "signed successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SignUp.this, "signed un-successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public boolean validation() {
+        try {
+            // Create a new user with a first, middle, and last name
+            Map<String, Object> user = new HashMap<>();
+            user.put("NAME", userName.getText().toString());
+            user.put("MAIL", email.getText().toString());
+            user.put("PHONE", phone.getText().toString());
+            user.put("PASSWORD", setPass.getText().toString());
+            FirebaseFirestore database = FirebaseFirestore.getInstance();
+            database.collection("userlist").document(userName.getText().toString())
+                    .set(user)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            isUser = true;
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            isUser = false;
+                        }
+                    });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return isUser;
     }
 }
