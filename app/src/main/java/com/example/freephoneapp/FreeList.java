24739efbc6d943b1d8377eba83deb9415999c85a
shararthkp47 <sharathkp18@gmail.com>
2020-11-22 17:48:09 +0530

    freelist done

--- app/src/main/java/com/example/freephoneapp/FreeList.java
+++ app/src/main/java/com/example/freephoneapp/FreeList.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,41 +24,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FreeList extends AppCompatActivity {
-TextView listTv;
-    @Override
+    List<String> list;
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_list);
-        listTv=findViewById(R.id.listtv);
-        listTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(FreeList.this,Main.class));
-            }
-        });
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        database.collection("userlist").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    List<String> list = new ArrayList<>();
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        list.add(document.getId());
+        try {
+            FirebaseFirestore database = FirebaseFirestore.getInstance();
+            database.collection("userlist").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        list = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            list.add(document.getId());
+                        }
+                        //list.remove("sharath");
+                    } else {
+                        Toast.makeText(FreeList.this, "You don't have any friends in list ...", Toast.LENGTH_SHORT).show();
                     }
-                 } else {
-                    //error
-                 }
-            }
-        });
+                    for (String userName : list) {
+                        addFriendToList(userName);
+                     }
+                }
+
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
-    public void addFriendToList(ArrayList userList){
+    public void addFriendToList(String userName){
              try {
                 LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 View rowView = inflater.inflate(R.layout.friend_list, null);
                 Button friendBtn = rowView.findViewById(R.id.friendbtn);
-                LinearLayout friendListView=findViewById(R.id.list_layout);
+                LinearLayout friendListView=findViewById(R.id.freelist);
                 friendListView.addView(rowView, friendListView.getChildCount());
-                friendBtn.setText(userList.get(0).toString());
+                friendBtn.setText(userName);
                  rowView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
